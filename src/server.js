0bb65cb44f3a1c3f6fcbabe2fa66235735aecfbc
Humberto Corrêa da Silva <humbertoc_silva@hotmail.com>
2023-01-18 19:10:53 -0300

    Example completed

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
+const crypto = require('crypto');
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const crypto = require('crypto');
-const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
 const jwt = require('jsonwebtoken');
 const jwksClient = require('jwks-rsa');
 const request = require('request-promise');
-const session = require('express-session');
 
 // loading env vars from .env file
 require('dotenv').config();
@@ -26,7 +26,7 @@ app.use(
     saveUninitialized: false
   })
 );
-app.engine('handlebars', handlebars());
+app.engine('handlebars', handlebars.engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -43,11 +43,96 @@ app.get('/profile', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.status(501).send();
+  // define constants for the authorization request
+  const authorizationEndpoint = oidcProviderInfo['authorization_endpoint'];
+  const responseType = 'id_token';
+  const scope = 'openid profile email';
+  const clientID = process.env.CLIENT_ID;
+  const redirectURI = 'http://localhost:3000/callback';
+  const responseMode = 'form_post';
+  const nonce = crypto.randomBytes(16).toString('hex');
+
+  // define a signed cookie containing the nonce value
+  const options = {
+    maxAge: 1000 * 60 * 15,
+    httpOnly: true, // The cookie only accessible by the web server
+    sined: true // Indicates if the cookie should be signed
+  };
+
+  // add cookie to the response and issue a 302 redirecting user
+  res
+    .cookie(nonceCookie, nonce, options)
+    .redirect(
+      authorizationEndpoint +
+        '?response_mode=' +
+        responseMode +
+        '&response_type=' +
+        responseType +
+        '&scope=' +
+        scope +
+        '&client_id=' +
+        clientID +
+        '&redirect_uri=' +
+        redirectURI +
+        '&nonce=' +
+        nonce
+    );
 });
 
 app.post('/callback', async (req, res) => {
-  res.status(501).send();
+  // take nonce from cookie
+  const nonce = req.cookies[nonceCookie];
+
+  // delete nonce
+  delete req.cookies[nonceCookie];
+
+  // take ID Token posted by the user
+  const { id_token } = req.body;
+
+  // decode token
+  const decodedToken = jwt.decode(id_token, { complete: true });
+
+  // get key id
+  const kid = decodedToken.header.kid;
+
+  // get public key
+  const client = jwksClient({
+    jwksUri: oidcProviderInfo['jwks_uri']
+  });
+
+  client.getSigningKey(kid, (err, key) => {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+
+    // verify signature and decode token
+    const verifiedToken = jwt.verify(id_token, signingKey);
+
+    // check audience, nonce, and expiration time
+    const {
+      nonce: decodedNonce,
+      aud: audience,
+      exp: expirationDate,
+      iss: issuer
+    } = verifiedToken;
+
+    const currentTime = Math.floor(Date.now() / 1000);
+    const expectedAudience = process.env.CLIENT_ID;
+
+    if (
+      audience !== expectedAudience ||
+      decodedNonce !== nonce ||
+      expirationDate < currentTime ||
+      issuer !== oidcProviderInfo['issuer']
+    ) {
+      // send and unauthorized http status
+      return res.status(401).send();
+    }
+
+    req.session.decodedIdToken = verifiedToken;
+    req.session.idToken = id_token;
+
+    // send the decoded version of the ID Token
+    res.redirect('profile');
+  });
 });
 
 app.get('/to-dos', async (req, res) => {
@@ -58,6 +143,18 @@ app.get('/remove-to-do/:id', async (req, res) => {
   res.status(501).send();
 });
 
-app.listen(3000, () => {
-  console.log(`Server running on http://localhost:3000`);
-});
+const { OIDC_PROVIDER } = process.env;
+const discoveryEndpoint = `https://${OIDC_PROVIDER}/.well-known/openid-configuration`;
+
+request(discoveryEndpoint)
+  .then(res => {
+    oidcProviderInfo = JSON.parse(res);
+    app.listen(3000, () => {
+      console.log(`Server running on http://localhost:3000`);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+    console.error(`Unable to get OIDC endpoints for ${OIDC_PROVIDER}`);
+    process.exit(1);
+  });
